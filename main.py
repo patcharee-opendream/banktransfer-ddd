b570931587e481ceeb698740e3a42b5075962804
phatcharee_p <phatcharee.pu@gmail.com>
2024-02-19 23:20:12 +0700

    connect database

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, MetaData, Table, select
+from databases import Database
+from sqlalchemy.sql import text
+
+DATABASE_URL = (
+    "postgresql://taejai_donation:taejai_donation@localhost:5432/taejai_donation"
+)
+engine = create_engine(DATABASE_URL)
+database = Database(DATABASE_URL)
+metadata = MetaData()
+
+
+# Reflect the donation table
+donation = Table("donations", metadata, autoload_with=engine)
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/donations/")
+async def read_donations():
+    query = select([donation])
+    result = await database.fetch_all(query)
+    return result
